3e9437a24ceae1ae739847cea684dc1b62267f80
konflic <konflic@mail.ru>
2022-01-10 22:33:17 +0300

    Версия 4.1

--- tests/test_pytest_intro.py
+++ tests/test_pytest_intro.py
@@ -1,20 +1,17 @@
-# Создание тестовых файлов
 def test_one():
     print(" >>> I'm test one!")
     assert 2 + 2 == 4
 
 
-# Так тоже можно, но не нужно и с модулем не заработает
-def testwo():
+def test_two():
     assert "Here" == "Where?"
 
 
-# Создание тестовых классов
 class TestClass:
     def test_one(self):
         list_to_test1 = []
         list_to_test2 = []
         assert list_to_test1 is list_to_test2
 
-    def testtwo(self):
+    def test_two(self):
         assert 1 is 1
