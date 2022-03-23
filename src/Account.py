f805e48ed3660117a17977d75f91e7d76d508666
konflic <konflic@mail.ru>
2022-03-23 10:22:55 +0300

    Версия 4.5

--- src/Account.py
+++ src/Account.py
@@ -4,12 +4,12 @@ import uuid
 class Account:
     CREATED = 0
 
-    def __init__(self, owner_id, balance):
+    def __init__(self, name, balance=0):
         self.__uid = str(uuid.uuid4())
-        self.__owner = owner_id
-        self.__set_balance(balance)
-        print("Account({}) was created!".format(self.__uid))
-        self.__increase_count()
+        self.__owner = name
+        self.__balance = 0
+        self.deposit(amount=balance)
+        self.__increase_accounts_count()
 
     @property
     def balance(self):
@@ -20,17 +20,12 @@ class Account:
         return self.__owner
 
     @classmethod
-    def __increase_count(cls):
+    def __increase_accounts_count(cls):
         cls.CREATED += 1
 
     @classmethod
-    def __decrease_count(cls):
+    def __decrease_accounts_count(cls):
         cls.CREATED -= 1
-
-    def __set_balance(self, amount):
-        if not isinstance(amount, int) and not isinstance(amount, float) or amount < 0:
-            amount = 0.0
-        self.__balance = amount
 
     def deposit(self, amount):
         if (isinstance(amount, int) or isinstance(amount, float)) and amount > 0:
@@ -39,22 +34,14 @@ class Account:
         return False
 
     def withdraw(self, amount):
-        if amount > 0 and (isinstance(amount, int) or isinstance(amount, float)):
-            self.__balance -= amount
-            return amount
+        if (isinstance(amount, int) or isinstance(amount, float)):
+            if amount > 0 and self.__balance > amount:
+                self.__balance -= amount
+                return amount
         return 0
 
     def transfer(self, other_account, amount):
-        other_account.deposit(self.withdraw(amount))
+        return other_account.deposit(self.withdraw(amount))
 
-    def close(self):
-        if self.__balance > 0:
-            print("Can't close not empty account!")
-            return False
-        else:
-            print("Account: {} was closed!".format(self))
-            del self
-            return True
-
-    def __repr__(self):
-        return "({}){}:{}$".format(self.__uid, self.owner, self.__balance)
+    def __del__(self):
+        self.__decrease_accounts_count()
--- tests/0_pytest_intro/test_real_code.py
+++ tests/0_pytest_intro/test_real_code.py
@@ -4,10 +4,24 @@ from src.Account import Account
 def test_account_create():
     a = Account("Mr.X", 0)
     assert isinstance(a, Account)
-    a.close()
-
-
-def test_default_not_activated():
-    a = Account("Mr.X", 0)
     assert a.balance == 0
-    a.close()
+    assert a.owner == "Mr.X"
+
+
+def test_account_with_balance_create():
+    a = Account("Mr.X", 100)
+    assert a.balance == 100
+
+
+def test_account_create_counter():
+    a = Account("Mr.X", 100)
+    b = Account("Mr.Y", 100)
+    assert a.CREATED, b.CREATED == (2, 2)
+
+
+def test_account_delete_counter():
+    a = Account("Mr.X", 100)
+    b = Account("Mr.Y", 100)
+    assert a.CREATED == 2
+    del b
+    assert a.CREATED == 1
